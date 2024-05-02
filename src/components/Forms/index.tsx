0e9b030b8b983663c832bb63e9fce688beeb8fe4
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-02 11:55:38 -0300

    usando o useState para capturar as informações do forms

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -3,18 +3,24 @@ import Botao from "../Botao";
 import style from './Forms.module.scss';
 
 class Formulario extends React.Component {
+
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
     render() {
         return(
             <>
                 <form className={ style.novaTarefa }>
                     <div className={ style.inputContainer }>
                         <label htmlFor="tarefa"> Adicione seu novo estudo: </label>
-                        <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
+                        <input type="text" name="tarefa" id="tarefa" value={this.state.tarefa} onChange={e=> this.setState({...this.state, tarefa: e.target.value})} placeholder="O que você quer estudar?" required />
                     </div>
 
                     <div className={ style.inputContainer }>
                         <label htmlFor="tempo"> Tempo: </label>
-                        <input type="time" name="tempo" step="1" id="tempo" min="00:00:00" max="99:59:59" required />
+                        <input type="time" name="tempo" step="1" id="tempo" min="00:00:00" value={this.state.tempo} onChange={e=> this.setState({...this.state, tempo: e.target.value})} max="99:59:59" required />
                     </div>
                     <Botao texto="Adicionar" />
                 </form>
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,24 +1,26 @@
 import React from "react";
+import { useState } from 'react';
 import style from './Lista.module.scss';
 import Item from "./Item/index";
 
+
 function Lista() {
 
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
     tarefa: 'HTML',
     tempo: '01:30:00'
-}, {
+    }, {
     tarefa: 'React',
     tempo: '03:00:00'
-}, {
+    }, {
     tarefa: 'Next.js',
     tempo: '02:30:00'
-}]
+}])
 
     return(
         <>
             <aside className={style.listaTarefas}>
-                <h2> Estudos do Dia: </h2>
+                <h2 onClick={() => setTarefas([...tarefas, {tarefa: "Estudar props", tempo: "05:00:00"}])}> Estudos do Dia: </h2>
                 <ul>
                     {tarefas.map((item, index) => (
                        <Item key={index} {...item} />
