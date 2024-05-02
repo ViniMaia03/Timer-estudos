810157e61eb0c6222c6307895fb670ac92bf5c1e
Vinicius <viniciusmsantos02@gmail.com>
2024-05-01 22:42:25 -0300

    estilização e criação dos modules.scss

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Botao from "../Botao";
-import './style.scss';
+import style from './Forms.module.scss';
 
 class Formulario extends React.Component {
     render() {
         return(
             <>
-                <form className="novaTarefa">
-                    <div className="inputContainer">
+                <form className={ style.novaTarefa }>
+                    <div className={ style.inputContainer }>
                         <label htmlFor="tarefa"> Adicione seu novo estudo: </label>
                         <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
                     </div>
 
-                    <div className="inputContainer">
+                    <div className={ style.inputContainer }>
                         <label htmlFor="tempo"> Tempo: </label>
                         <input type="time" name="tempo" step="1" id="tempo" min="00:00:00" max="01:30:00" required />
                     </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,13 @@
-import React from "react"
-import Botao from "../components/Botao"
-import Formulario from "../components/Forms"
-import Lista from "../components/Lista"
-import './style.scss';
+import React from "react";
+import Botao from "../components/Botao";
+import Formulario from "../components/Forms";
+import Lista from "../components/Lista";
+import style from './App.module.scss';
 
 export default function App() {
     return (
         <>
-            <div className="AppStyle">
+            <div className={style.AppStyle}>
                 <Formulario />
                 <Lista />
             </div>
